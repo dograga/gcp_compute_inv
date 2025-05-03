15d7608b13b03048e4774a7fb7d91a9b008c09c6
gaurav.dogra@gmail.com <gaurav.dogra@gmail.com>
2025-05-03 12:27:28 +0800

    Test

---
+++ gke/main.py
@@ -0,0 +1,61 @@
+from google.cloud import container_v1
+from google.auth import default
+from dataclasses import dataclass, asdict
+import json
+from typing import List, Optional
+import redis
+
+@dataclass
+class GKEClusterInfo:
+    name: str
+    location: str
+    status: str
+    node_count: int
+    master_version: str
+    endpoint: str
+    project_id: str
+    node_version: Optional[str] = None
+
+def fetch_gke_clusters() -> List[GKEClusterInfo]:
+    credentials, project_id = default()
+    client = container_v1.ClusterManagerClient(credentials=credentials)
+    parent = f"projects/{project_id}/locations/-"  # All regions
+    response = client.list_clusters(parent=parent)
+    clusters = response.clusters
+
+    if not clusters:
+        print("No clusters found.")
+        return []
+    cluster_list: List[GKEClusterInfo] = []
+
+    for cluster in clusters:
+            info = GKEClusterInfo(
+                name=cluster.name,
+                location=cluster.location,
+                status=cluster.status.name,
+                node_count=cluster.current_node_count,
+                master_version=cluster.current_master_version,
+                endpoint=cluster.endpoint,
+                project_id=project_id
+            )
+            cluster_list.append(info)
+    return cluster_list
+
+
+def main():
+    clusters = fetch_gke_clusters()
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    inventory_key = "gke:clusters"
+    if not clusters:
+        print("No GKE clusters found.")
+    else:
+        for cluster in clusters:
+            print(cluster)
+            cluster_key = f"gke:cluster:{cluster.name}"
+            r.set(cluster_key, json.dumps(asdict(cluster)))
+            r.sadd(inventory_key, cluster.name)
+            # Connect to Redis
+
+
+if __name__ == "__main__":
+    main()
