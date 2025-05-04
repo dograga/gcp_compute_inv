00788bb10d3f9ea946cd39df94f1ea5e0bae5993
gaurav.dogra@gmail.com <gaurav.dogra@gmail.com>
2025-05-04 12:08:16 +0800

    Test

--- gke/main.py
+++ gke/main.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass, asdict
 import json
 from typing import List, Optional
 import redis
+import structlog
+#import app.log_config as _
+
+logger = structlog.get_logger()
 
 @dataclass
 class GKEClusterInfo:
@@ -18,6 +22,8 @@ class GKEClusterInfo:
 
 def fetch_gke_clusters() -> List[GKEClusterInfo]:
     credentials, project_id = default()
+    logger.info("Using project_id", project_id=project_id)
+    logger.info("Using credentials", credentials=credentials)
     client = container_v1.ClusterManagerClient(credentials=credentials)
     parent = f"projects/{project_id}/locations/-"  # All regions
     response = client.list_clusters(parent=parent)
@@ -25,6 +31,8 @@ def fetch_gke_clusters() -> List[GKEClusterInfo]:
 
     if not clusters:
         print("No clusters found.")
+        logger.info("No clusters found.")
+        # Optionally, you can raise an exception or return an empty list
         return []
     cluster_list: List[GKEClusterInfo] = []
 
@@ -52,6 +60,7 @@ def main():
     else:
         for cluster in clusters:
             print(cluster)
+            logger.info("Cluster info", cluster=cluster)
             cluster_key = f"gke:cluster:{cluster.name}"
             r.set(cluster_key, json.dumps(asdict(cluster)))
             r.sadd(inventory_key, cluster.name)
